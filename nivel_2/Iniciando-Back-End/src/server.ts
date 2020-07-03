d379d7dc926b7e6732cf61ea4df32b963515b8fe
iybe <iesleybezerra2@gmail.com>
2020-07-03 20:58:19 -0300

    aulas backend

--- nivel_2/Iniciando-Back-End/src/server.ts
+++ nivel_2/Iniciando-Back-End/src/server.ts
@@ -1,14 +1,16 @@
 import 'reflect-metadata';
+
 import express from 'express';
 import routes from './routes';
+import uploadConfig from './config/upload';
 
 import './database/index';
 
 const app = express();
 
 app.use(express.json());
-
-app.use('/', routes);
+app.use('/files', express.static(uploadConfig.directory));
+app.use(routes);
 
 app.listen(3333,() => {
   console.log("Server in http://localhost:3333");
